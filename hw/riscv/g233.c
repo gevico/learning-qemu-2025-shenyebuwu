147faeffecef792ac680266305ffc944b308d843
shenyebuwu <shenyebuwu@gmail.com>
2025-10-28 07:44:24 +0000

    feat: test1

--- hw/riscv/g233.c
+++ hw/riscv/g233.c
@@ -49,10 +49,13 @@ static const MemMapEntry g233_memmap[] = {
 
 static void g233_soc_init(Object *obj)
 {
-    /*
-     * You can add more devices here(e.g. cpu, gpio)
-     * Attention: The cpu resetvec is 0x1004
-     */
+    G233SoCState *s = RISCV_G233_SOC(obj);
+
+    /* 初始化cpu实例 */
+    object_initialize_child(obj, "cpus", &s->cpus, TYPE_RISCV_HART_ARRAY);
+
+    /* 初始化gpio实例 */
+    object_initialize_child(obj, "gpio0", &s->gpio, TYPE_SIFIVE_GPIO);
 }
 
 static void g233_soc_realize(DeviceState *dev, Error **errp)
@@ -63,6 +66,12 @@ static void g233_soc_realize(DeviceState *dev, Error **errp)
     const MemMapEntry *memmap = g233_memmap;
 
     /* CPUs realize */
+    qdev_prop_set_uint32(DEVICE(&s->cpus), "num-harts", ms->smp.cpus);
+    qdev_prop_set_uint32(DEVICE(&s->cpus), "hartid-base", 0);
+    qdev_prop_set_string(DEVICE(&s->cpus), "cpu-type", TYPE_RISCV_CPU_GEVICO_G233);
+    qdev_prop_set_uint64(DEVICE(&s->cpus), "resetvec", 0x1004);
+
+    sysbus_realize(SYS_BUS_DEVICE(&s->cpus), &error_fatal);
 
     /* Mask ROM */
     memory_region_init_rom(&s->mask_rom, OBJECT(dev), "riscv.g233.mrom",
@@ -160,10 +169,14 @@ static void g233_machine_init(MachineState *machine)
     }
 
     /* Initialize SoC */
+    object_initialize_child(OBJECT(machine), "soc", &s->soc, TYPE_RISCV_G233_SOC);
 
+    qdev_realize(DEVICE(&s->soc), NULL, &error_fatal);
 
     /* Data Memory(DDR RAM) */
-
+    memory_region_add_subregion(get_system_memory(),
+                                memmap[G233_DEV_DRAM].base,
+                                machine->ram);
     /* Mask ROM reset vector */
     uint32_t reset_vec[5];
     reset_vec[1] = 0x0010029b; /* 0x1004: addiw  t0, zero, 1 */
